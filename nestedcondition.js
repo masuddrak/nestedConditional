832c8fea2954e1bbe02c5c98e9544df39af2c9a5
masudrana <masudranaacb@gmail.com>
2022-01-28 00:34:35 +0600

    frist nested commited

---
+++ nestedcondition.js
@@ -0,0 +1,17 @@
+var cha=10;
+var chaBiskut=50;
+var chaBiskutDud=50;
+var lalCha=5;
+var myMony=10;
+if(cha<myMony){
+    console.log("cha khobo sudhu");
+}
+else if(chaBiskut<myMony){
+    console.log(" cha dis biskut khabo");
+}
+else if(chaBiskutDud<myMony){
+    console.log("cha dia dud biskut khabo mmoja kore");
+}
+else{
+    console.log("just lal cha khabo");
+}
